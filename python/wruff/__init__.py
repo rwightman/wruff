0799dcfd8a2bc71b6b08681842e23119d92ed3ce
Ross Wightman <rwightman@gmail.com>
2026-04-17 10:55:19 -0700

    More README and metadata changes wrt to wruff vs ruff

--- python/wruff/__init__.py
+++ python/wruff/__init__.py
@@ -2,4 +2,4 @@ from __future__ import annotations
 
 from ._find_wruff import find_ruff_bin, find_wruff_bin
 
-__all__ = ["find_wruff_bin", "find_ruff_bin"]
+__all__ = ["find_ruff_bin", "find_wruff_bin"]
